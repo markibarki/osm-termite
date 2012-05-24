223dc9ffc6b5bd4323da8bf8e931dc4b3395cf10
sutter@intransix.com <sutter@intransix.com@af764aba-67e7-5820-30b9-39aad98315e9>
2012-05-24 00:35:32 +0000

    more feature enhancement and modifications for importing and rendering

--- trunk/Termite/src/intransix/osm/termite/map/geom/Feature.java
+++ trunk/Termite/src/intransix/osm/termite/map/geom/Feature.java
@@ -1,6 +1,7 @@
 package intransix.osm.termite.map.geom;
 
 import intransix.osm.termite.map.MapObject;
+import intransix.osm.termite.map.prop.FeatureInfo;
 import intransix.osm.termite.theme.Style;
 import java.awt.Graphics2D;
 
@@ -8,7 +9,7 @@ import java.awt.Graphics2D;
  * This encapsulates a general map feature within termite (not osm)
  * @author sutter
  */
-public abstract class Feature extends MapObject {
+public abstract class Feature extends MapObject implements Comparable<Feature> {
 	
 	//====================
 	// Constants
@@ -19,12 +20,15 @@ public abstract class Feature extends MapObject {
 		POINT, LINE, MULTILINE, AREA, MULTIAREA
 	}
 	
+	public final static int DEFAULT_ZORDER = 999;
+	
 	//====================
 	// Private Proeprties
 	//====================
 
 	private FeatureType featureType;
 	private Style style;
+	private FeatureInfo featureInfo;
 	
 	//====================
 	// Public Methods
@@ -45,6 +49,14 @@ public abstract class Feature extends MapObject {
 	public Style getStyle() {
 		return style;
 	}
+	
+	public void setFeatureInfo(FeatureInfo featureInfo) {
+		this.featureInfo = featureInfo;
+	}
+	
+	public FeatureInfo getFeatureInfo() {
+		return featureInfo;
+	}
 
 	/** This clears the style value. */
 	public void clearStyle() {
@@ -53,6 +65,25 @@ public abstract class Feature extends MapObject {
 	
 	/** renders the feature. */
 	public abstract void render(Graphics2D g);
+	
+	@Override
+	public int compareTo(Feature other) {
+		int otherZorder;
+		int thisZorder;
+		if(other.featureInfo == null) {
+			otherZorder = FeatureInfo.DEFAULT_ZORDER;
+		}
+		else {
+			otherZorder = other.featureInfo.getZorder();
+		}
+		if(this.featureInfo == null) {
+			thisZorder = FeatureInfo.DEFAULT_ZORDER;
+		}
+		else {
+			thisZorder = this.featureInfo.getZorder();
+		}
+		return thisZorder - otherZorder;	
+	}
 	
 	//====================
 	// Public Methods
