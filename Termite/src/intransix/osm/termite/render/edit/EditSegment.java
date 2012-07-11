e91b6f5ed055b67b77a81055e0ece45a75366107
sutter <sutter@intransix.com>
2012-07-11 03:27:06 +0000

    added snapping to the vertical and horizontal and to perpindicular. need to clean up!!! The edit layer is ugly!

--- Termite/src/intransix/osm/termite/render/edit/EditSegment.java
+++ Termite/src/intransix/osm/termite/render/edit/EditSegment.java
@@ -1,5 +1,7 @@
 package intransix.osm.termite.render.edit;
 
+import intransix.osm.termite.map.data.OsmSegment;
+
 /**
  *
  * @author sutter
@@ -7,9 +9,11 @@ package intransix.osm.termite.render.edit;
 public class EditSegment {
 	public EditNode en1;
 	public EditNode en2;
+	public OsmSegment osmSegment;
 	
-	public EditSegment(EditNode en1, EditNode en2) {
+	public EditSegment(EditNode en1, EditNode en2, OsmSegment osmSegment) {
 		this.en1 = en1;
 		this.en2 = en2;
+		this.osmSegment = osmSegment;
 	}	
 }
