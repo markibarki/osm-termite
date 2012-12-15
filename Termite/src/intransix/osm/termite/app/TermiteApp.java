5964482352c829d15c9166084f5cd979f350e936
sutter <sutter@intransix.com>
2012-12-15 00:09:47 +0000

    iterated version for next release

--- Termite/src/intransix/osm/termite/app/TermiteApp.java
+++ Termite/src/intransix/osm/termite/app/TermiteApp.java
@@ -40,7 +40,7 @@ public class TermiteApp {
 	//this can be overridden using a command line argument
 	private static String CONFIG_FILE_NAME = "config.json";
 	
-	private final static String VERSION = "0.0.8p0";
+	private final static String VERSION = "0.0.8p1";
 	
 	private TermiteGui gui;
 	private LoginManager loginManager = new LoginManager();
