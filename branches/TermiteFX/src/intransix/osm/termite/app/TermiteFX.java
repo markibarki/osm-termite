2b296af361088f0ad25113f435ad0df10ec80f9d
sutter@intransix.com <sutter@intransix.com@af764aba-67e7-5820-30b9-39aad98315e9>
2013-04-19 23:19:58 +0000

    nothing significant

--- branches/TermiteFX/src/intransix/osm/termite/app/TermiteFX.java
+++ branches/TermiteFX/src/intransix/osm/termite/app/TermiteFX.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package intransix.osm.termite.app;
 
 import intransix.osm.termite.app.basemap.BaseMapManager;
